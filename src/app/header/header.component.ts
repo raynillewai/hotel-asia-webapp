2c87edfa491531e148920756e5f11d3ede31dc3a
Wai Xin May <S012510@touchngo.com.my>
2020-08-25 11:51:54 +0800

    fixed routerlink issue

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  sub: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.sub = this.route.params.subscribe(
+      params => console.log(this.route.params)
+    );
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  gotoListing() {
+    this.router.navigateByUrl('hotel-packages', { replaceUrl: true });
   }
 
 }
